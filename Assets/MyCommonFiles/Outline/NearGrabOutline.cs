984621406681d9622927ebf770ababadc32c2c21
SirotenkoMax <sirotenko.max1998@gmail.com>
2020-09-20 19:44:01 +0300

    CompletedBottle

--- Assets/MyCommonFiles/Outline/NearGrabOutline.cs
+++ Assets/MyCommonFiles/Outline/NearGrabOutline.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class NearGrabOutline : MonoBehaviour
 {
-
+    [SerializeField] OVRInput.Button button;
     void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<OutlineObject>())
         {
             other.GetComponent<OutlineObject>().EnableOutline = true;
+        }
+    }
+
+    //Userfull for ChemLab project only
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponent<DistroyConnection>())
+        {
+            if (other.GetComponent<Animator>())
+            {
+                if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) || OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+                {
+                    other.GetComponent<Animator>().Play("Cup");
+                }
+            }            
         }
     }
 
--- Assets/Obi/Sample Scenes/DistroyConnection.cs
+++ Assets/Obi/Sample Scenes/DistroyConnection.cs
@@ -5,19 +5,41 @@ using UnityEngine;
 public class DistroyConnection : MonoBehaviour
 {
     public Transform NewParent;
-
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (gameObject.GetComponent<OVRGrabbable>().isGrabbed)
+        //Dont use it, but keep just in case
+        /*if (gameObject.GetComponent<OVRGrabbable>().isGrabbed)
         {
-            transform.parent = NewParent;
+            if (NewParent)
+            {
+                transform.parent = NewParent;
+            }
+            if (GetComponent<Joint>())
+            {
+                Destroy(GetComponent<Joint>());
+            }
+        }*/
+    }
+
+    public void DestroyAfterAnimation()
+    {
+        if (NewParent)
+        {
+            //Destroy(GetComponent<Animator>());
+            //Vector3 pos = transform.position; //Это костыль
+            //gameObject.transform.SetParent(null, true);
+            //transform.position = pos; // И это костыль, но он улетает после смены родятеля, хз как это изменить(((
+            //gameObject.AddComponent<Rigidbody>();
+            Destroy(gameObject);
         }
     }
+
 }
